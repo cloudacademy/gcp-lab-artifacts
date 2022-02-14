11ac85b0ff7a59820a4d225ed87756e831c455d4
Jun <jun.fritz@gmail.com>
2022-02-14 13:43:23 -1000

    starter main.py file.

---
+++ cloud-function-code/firestore/main.py
@@ -0,0 +1,19 @@
+from google.cloud import firestore
+
+def add_document(event, context):
+    db = firestore.Client(project='my-project-id')
+    doc_ref = db.collection(u'users').document(u'alovelace')
+    doc_ref.set({
+        u'first': u'Ada',
+        u'last': u'Lovelace',
+        u'born': 1815
+    })
+
+def get_collection(event, context):
+    db = firestore.Client(project='my-project-id')
+    users_ref = db.collection(u'users')
+    docs = users_ref.stream()
+
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()}')
+
