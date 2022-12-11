6cfe139c1eff29702c46f0a09b8af6aaa7300231
lrakai <lmrakai@gmail.com>
2022-12-10 19:11:09 -0700

    remove sa output

--- challenges/operations/tf/output.tf
+++ challenges/operations/tf/output.tf
@@ -10,7 +10,3 @@ output "repository_http_url" {
   description = "HTTP URL of the repository in Cloud Source Repositories."
   value       = google_sourcerepo_repository.repo.url
 }
-
-output "google_service_account_email" {
-  value = google_service_account.sa.email
-}
