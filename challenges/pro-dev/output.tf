85e11c3fa81b7232c668e7233c843d4b78f76242
lrakai <lmrakai@gmail.com>
2023-07-11 12:08:35 -0600

    Fix output

--- challenges/pro-dev/output.tf
+++ challenges/pro-dev/output.tf
@@ -6,7 +6,7 @@ output "url" {
   value = "${google_cloud_run_service.my-service.status[0].url}"
 }
 
-output "repository_http_url" {
+output "api_repository_http_url" {
   description = "HTTP URL of the repository in Cloud Source Repositories."
-  value       = google_sourcerepo_repository.repo.url
+  value       = google_sourcerepo_repository.api_repo.url
 }
