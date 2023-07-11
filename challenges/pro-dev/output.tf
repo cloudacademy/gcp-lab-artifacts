70c7e74fd7ea2a026450142c6f68eb24b53a6a94
lrakai <lmrakai@gmail.com>
2023-07-11 12:11:30 -0600

    comment run output

--- challenges/pro-dev/output.tf
+++ challenges/pro-dev/output.tf
@@ -2,9 +2,9 @@ output "project_id" {
   value = var.project_name
 }
 
-output "url" {
-  value = "${google_cloud_run_service.my-service.status[0].url}"
-}
+# output "url" {
+#   value = "${google_cloud_run_service.my-service.status[0].url}"
+# }
 
 output "api_repository_http_url" {
   description = "HTTP URL of the repository in Cloud Source Repositories."
